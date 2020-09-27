080da480764e8ec312786cd7e86a1835dc29d975
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2020-09-27 20:50:41 +0200

    Created the different files to be loaded into the iframe
    The pages have been created and filled with a placeholder. The next step is linking to these pages by adding functionality to the (now useless) buttons.

--- app.py
+++ app.py
@@ -11,12 +11,28 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def home(iframe="landing_iframe"):
+    return render_template("home.html", iframe = iframe)
 
-@app.route('/landing_iframe.html')
+@app.route('/previews/landing_iframe.html')
 def landing_iframe():
-    return render_template('landing_iframe.html')
+    return render_template('/previews/landing_iframe.html')
 
-if __name__ == '__main__':
+@app.route("/previews/cards.html")
+def preview_cards():
+    return render_template("/previews/cards.html")
+
+@app.route("/previews/characters.html")
+def preview_characters():
+    return render_template("/previews/characters.html")
+
+@app.route("/previews/text.html")
+def preview_text():
+    return render_template("/previews/text.html")
+
+@app.route("/previews/instructions.html")
+def instructions():
+    return render_template("/previews/instructions.html")
+
+if __name__ == "__main__":
     app.run()
